45e382d2f54cf3628bffccf5d3fb73acc83d3bc5
MacMini2020 <evan_daniel@Evans-Mac-mini.local>
2022-10-08 16:26:47 -0500

    a few homepage elements

--- homepage.js
+++ homepage.js
@@ -1,3 +1,5 @@
-window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
-    
+window.addEventListener('DOMContentLoaded', () => {
+    const cont = document.querySelector('canvas').getContext('2d'); 
+    cont.canvas.width = window.devicePixelRatio * cont.canvas.clientWidth; 
+    cont.canvas.height = window.devicePixelRatio * cont.canvas.clientHeight; 
 }); 
