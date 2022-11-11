4e776a19707df592fe624c5ea197059cdb8fa441
evan daniel <evan.daniel@sckans.edu>
2022-11-10 18:33:36 -0600

    mjs

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-// import express from 'express'; 
-// import dotenv from 'dotenv'; 
+import express from 'express'; 
+import dotenv from 'dotenv'; 
 // import { Configuration, OpenAIApi } from "openai";
 
-const express = require('express'); 
-const dotenv = require('dotenv'); 
+// const express = require('express'); 
 
+// const dotenv = require('dotenv'); 
 dotenv.config(); 
 
 // const configuration = new Configuration({
@@ -21,21 +21,23 @@ app.use(express.json());
 app.post('/api/generate', async (req, res) => {
     console.log('POST', req.body.desc); 
 
-    // const img = await openai.createImage({
-    //   prompt: req.body.desc,
-    //   n: 1,
-    //   size: "512x512",
-    // });
-    // res.status(200).json({ result: img.data.data[0].url }); 
+    const img = await openai.createImage({
+      prompt: req.body.desc,
+      n: 1,
+      size: "512x512",
+    });
+    // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.status(200).json({ result: img.data.data[0].url }); 
     
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.status(200).json({ result: 'PHLEGMER' }); 
+    // TESTING
+    // SEND A RESULT WE KNOW WE WILL SUCCEED
+    // res.status(200).json({ result: 'PHLEGMER' }); 
     
 }); 
 
 // app.use(express.static('public')); 
 // app.listen(3000, () => console.log('Listening on port 3000.'));
 
-// export default app; 
+export default app; 
 
-module.exports = app; 
+// module.exports = app; 
